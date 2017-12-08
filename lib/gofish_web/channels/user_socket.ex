3425afa26d5a7fb38285977cda5144388c7fc757
EssenceOfChaos <fjschiller@gmail.com>
2017-12-08 18:45:06 -0500

    configure for heroku deployment

--- lib/gofish_web/channels/user_socket.ex
+++ lib/gofish_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule GofishWeb.UserSocket do
   # channel "room:*", GofishWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
